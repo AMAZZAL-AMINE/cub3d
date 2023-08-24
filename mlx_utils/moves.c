24346455c5153507b52abab31533648f302e3835
Mohamed Amine Amazzal <mamazzal@c1r3p3.1337.ma>
2023-08-24 16:11:07 +0100

    int

--- mlx_utils/moves.c
+++ mlx_utils/moves.c
@@ -6,17 +6,19 @@
 /*   By: mamazzal <mamazzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 10:29:49 by mamazzal          #+#    #+#             */
-/*   Updated: 2023/08/24 15:10:27 by mamazzal         ###   ########.fr       */
+/*   Updated: 2023/08/24 16:08:54 by mamazzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3D.h"
 
+#define MOVE_SPEED 0.05
+
 void move_up(t_vars *vars)
 {
 	if (vars->key == 13 || vars->key == 126)
 	{
-		vars->p_pos_y = vars->p_pos_y - 0.2;
+		vars->p_pos_y = vars->p_pos_y - MOVE_SPEED;
 		printf("POSITION X => %f\n", vars->p_pos_x);
 		printf("POSITION Y => %f\n", vars->p_pos_y);
 	}
@@ -26,7 +28,7 @@ void move_lef(t_vars *vars)
 {
 	if (vars->key == 0 || vars->key == 123)
 	{
-		vars->p_pos_x = vars->p_pos_x - 0.2;
+		vars->p_pos_x = vars->p_pos_x - MOVE_SPEED;
 		printf("POSITION X => %f\n", vars->p_pos_x);
 		printf("POSITION Y => %f\n", vars->p_pos_y);
 	}
@@ -36,7 +38,7 @@ void move_down(t_vars *vars)
 {
 	if (vars->key == 1 || vars->key == 125)
 	{
-		vars->p_pos_y = vars->p_pos_y + 0.2;
+		vars->p_pos_y = vars->p_pos_y + MOVE_SPEED;
 		printf("POSITION X => %f\n", vars->p_pos_x);
 		printf("POSITION Y => %f\n", vars->p_pos_y);
 	}
@@ -46,7 +48,7 @@ void move_right(t_vars *vars)
 {
 	if (vars->key == 2 || vars->key == 124)
 	{
-		vars->p_pos_x = vars->p_pos_x + 0.2;
+		vars->p_pos_x = vars->p_pos_x + MOVE_SPEED;
 		printf("POSITION X => %f\n", vars->p_pos_x);
 		printf("POSITION Y => %f\n", vars->p_pos_y);
 	}
