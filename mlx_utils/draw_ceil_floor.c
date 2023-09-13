82f16d77a49f8230cbdc2b17e192a2621bcff468
Mohamed Amine Amazzal <mamazzal@c1r3p3.1337.ma>
2023-09-13 13:02:01 +0100

    wat the hell ami doing here

--- mlx_utils/draw_ceil_floor.c
+++ mlx_utils/draw_ceil_floor.c
@@ -6,7 +6,7 @@
 /*   By: mamazzal <mamazzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 18:16:40 by mamazzal          #+#    #+#             */
-/*   Updated: 2023/09/13 12:51:37 by mamazzal         ###   ########.fr       */
+/*   Updated: 2023/09/13 12:57:19 by mamazzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,15 @@
 void	draw_ceil(t_vars *vars)
 {
 	int count = 0;
+	char **splite = ft_split(vars->pars_data->rgb->ceil[1], ',');
+	int r = ft_atoi(splite[0]);
+	int g = ft_atoi(splite[1]);
+	int b = ft_atoi(splite[2]);
+	free_double(splite);
 	while (count < vars->dis.h / 2) {
 		int index = 0;
 		while (index < vars->dis.w) {
-			my_mlx_pixel_put(vars, index, count, create_trgb(115,202,229));
+			my_mlx_pixel_put(vars, index, count, create_trgb(r,g,b));
 			index++;
 		}
 		count++;
@@ -28,10 +33,15 @@ void	draw_ceil(t_vars *vars)
 void	draw_floor(t_vars *vars)
 {
 	int count = vars->dis.h / 2;
+	char **splite = ft_split(vars->pars_data->rgb->floor[1], ',');
+	int r = ft_atoi(splite[0]);
+	int g = ft_atoi(splite[1]);
+	int b = ft_atoi(splite[2]);
+	free_double(splite);
 	while (count < vars->dis.h) {
 		int index = 0;
 		while (index < vars->dis.w) {
-			my_mlx_pixel_put(vars, index, count, create_trgb(120,73,55));
+			my_mlx_pixel_put(vars, index, count, create_trgb(r,g,b));
 			index++;
 		}
 		count++;
